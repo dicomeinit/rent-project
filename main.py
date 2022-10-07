a80c5b1fc0a1134ff413481fbc1d423d965e38e0
Diana <dicomeinit@gmail.com>
2022-10-08 00:01:19 +0300

    Create a list of addresses for all the listings

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 "https://docs.google.com/forms/d/e/1FAIpQLScaXLbp4gz0Lozl094xxPo-TRkYQWH0BrBG7CAOk0IPEpov-A/viewform?usp=sf_link"
+from pprint import pprint
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,13 +13,12 @@ header = {
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
 
-response = requests.get("https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D", headers=header)
+response = requests.get(URL, headers=header)
 # print(response.raise_for_status)
 data = response.text
 soup = BeautifulSoup(data, "html.parser")
 
-all_link_elements = soup.select(".property-card-data a")
-# print(all_link_elements)
+all_link_elements = soup.select(".property-card-link")
 
 all_links = []
 for link in all_link_elements:
@@ -30,3 +31,15 @@ for link in all_link_elements:
 
 print(all_links)
 print(len(all_links))
+
+
+# =============== Create a list of addresses for all the listings you scraped. e.g. =========
+
+all_address_elements = soup.select(".property-card-link address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+print(all_addresses)
+print(len(all_addresses))
+
+
+# <span data-test="property-card-price">$2,305+ 1 bd</span>
+# <address data-test="property-card-addr">Woodchase Apartment Homes | 2795 San Leandro Blvd, San Leandro, CA</address>
